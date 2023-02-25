53168d3ebbc9bebc3a83edf9683492ffed76102c
Yuvraj Singh <yuvraj0157@gmail.com>
2023-02-25 14:23:19 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const express = require('express');
 const path = require('path');
 
 // Express setup
-const PORT = process.env.PORT || 3000;
+// const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 const app = express();
 
 // View engine
