6521dccf7a3b6ee615064185190fc4fbfd1aafda
Yuvraj Singh <yuvraj0157@gmail.com>
2023-02-25 12:35:46 +0530

    Update connection.js

--- app/config/connection.js
+++ app/config/connection.js
@@ -1,17 +1,13 @@
 const mysql = require('mysql');
 require('dotenv').config();
-let connection;
 
-if(process.env.JAWSDB_URL) {
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-    connection = mysql.createConnection({
-        host: process.env.DBHOST,
-        user: process.env.DBUSER,
-        password: process.env.DBPASSWORD,
-        database: process.env.DBNAME,
-    });
-}
+var connection = mysql.createConnection({
+    host: process.env.DBHOST,
+    user: process.env.DBUSER,
+    password: process.env.DBPASSWORD,
+    database: process.env.DBNAME,
+    multipleStatements : true
+});
 
 connection.on('connect', () => {
     console.log(`--> Connected to database '${connection.config.database}'.`);
