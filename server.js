f90c59883056dcd843aedceee262ce1fff92b27b
Yuvraj Singh <yuvraj0157@gmail.com>
2024-04-15 18:03:49 +0530

    Update PORT variable in server.js to use environment variable

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const todoRoutes = require('./routes/todo');
 require('dotenv').config();
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 const app = express();
 
 
